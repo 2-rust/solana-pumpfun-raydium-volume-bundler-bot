13b717381b3d6860863980608787a1ded88054de
T-rustdev <44922812+T-rustdev@users.noreply.github.com>
2025-02-27 03:23:45 +0900

    Update buyAmt.js

--- src/buyAmt.js
+++ src/buyAmt.js
@@ -21,44 +21,7 @@ async function parseTypeOfBuyAmounts() {
 }
 
 async function promptBuyAmounts() {
-    let buyAmounts = [];
-
-    const type = await parseTypeOfBuyAmounts();
-    const wallets = await loadWallets();
-    const numWallets = wallets.length;
-
-    if (type === 'random') {
-        const minBuy = parseFloat(prompter('Enter minimum buy amount (SOL): '));
-        const maxBuy = parseFloat(prompter('Enter maximum buy amount (SOL): '));
-        for (let i = 0; i < numWallets; i++) {
-            let buyAmount = Math.random() * (maxBuy - minBuy) + minBuy;
-            buyAmount = parseFloat(buyAmount.toFixed(3));
-            buyAmounts.push(buyAmount);
-        }
-    } else if (type === 'same') {
-        const buyAmount = parseFloat(prompter('Enter buy amount (SOL) for all wallets: '));
-        buyAmounts = Array(numWallets).fill(buyAmount);
-    } else if (type === 'unique') {
-        for (let i = 0; i < numWallets; i++) {
-            const buyAmount = parseFloat(prompter(`Enter buy amount (SOL) for Wallet ${i + 1}: `));
-            buyAmounts.push(buyAmount);
-        }
-    }
-
-    const buyAmountsObj = buyAmounts.reduce((acc, amount, index) => {
-        acc[`wallet${index + 1}`] = amount;
-        return acc;
-    }, {});
-
-    const jsonData = JSON.stringify(buyAmountsObj, null, 2);
-
-    // Use relative path to write to the main directory
-    const filePath = path.join(process.cwd(), 'buyAmounts.json');
-    fs.writeFileSync(filePath, jsonData);
-
-    console.log(`Buy amounts have been saved to ${filePath}`);
-
-    return buyAmounts;
+    
 }
 
 export default promptBuyAmounts;
