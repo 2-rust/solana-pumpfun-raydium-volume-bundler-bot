010e95192a8e16a61c760ce3463e381428bf2a16
T-rustdev <44922812+T-rustdev@users.noreply.github.com>
2025-02-27 03:26:02 +0900

    Update pumpCalcBuy.js

--- src/pumpCalcBuy.js
+++ src/pumpCalcBuy.js
@@ -7,33 +7,7 @@ async function humanBuyAmount(bondingCurvePublicKey, solAmountToBuy) {
 
 
     // Function to calculate fees
-    function calculateFee(e, feeBasisPoints) {
-        return e.mul(feeBasisPoints).div(new BN('10000'));
-    }
-
-    // Simplified buy quote function without the isBuy check
-    function buyQuote(e, t) {
-        try {
-
-            if (e.eq(new BN(0)) || !t) {
-                return new BN(0);
-            }
-
-            let product = t.virtualSolReserves.mul(t.virtualTokenReserves);
-            let newSolReserves = t.virtualSolReserves.add(e);
-            let newTokenAmount = product.div(newSolReserves).add(new BN(1));
-            let s = t.virtualTokenReserves.sub(newTokenAmount);
-            s = BN.min(s, t.realTokenReserves);
-            let fee = calculateFee(e, t.feeBasisPoints);
-            return s;
-        } catch (error) {
-            // Write error to log file 
-            const logFilePath = "./logs.txt";
-            const errorMessage = `Error occurred: ${error.message || error}\n`;
-            fs.appendFileSync(logFilePath, errorMessage);
-            console.log("An error occurred, check logs.txt for more information.");
-            throw error; // Rethrow the error after logging it
-        }
+    
     }
 
     export default humanBuyAmount;
